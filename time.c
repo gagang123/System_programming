162f64df068a2f9cfdf27acc22094e56197d90aa
Gagandeep Garg <gagandeepgarg6@gmail.com>
2016-10-03 23:24:03 -0300

    Add files via upload

---
+++ time.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	struct timeval tv;
+	double t1;
+	double t2;
+
+	gettimeofday(&tv, NULL);
+	t1 = tv.tv_sec + tv.tv_usec/1000000.0;
+
+	sleep(1);
+	usleep(50);
+
+	gettimeofday(&tv, NULL);
+	t2 = tv.tv_sec + tv.tv_usec/1000000.0;
+
+	printf("%.6lf seconds elapsed.\n", t2-t1);
+
+	return 0;
+
+}
